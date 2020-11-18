566d3b8946ca3444ca6b5b625831049780a59f44
lalebdi <67381036+lalebdi@users.noreply.github.com>
2020-11-18 16:24:23 -0500

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -23,7 +23,6 @@ export function register(config) {
 
         checkValidServiceWorker(swUrl, config);
 
-
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
